7f1a47a5df6e539909d6b5cde3db8d0b31d25fe2
Nicola-Controne <109146662+Nicola-Controne@users.noreply.github.com>
2022-07-29 06:14:24 -0400

    Add files via upload

--- port_scanner.py
+++ port_scanner.py
@@ -1,13 +1,13 @@
 import socket
 
 indirizzo_ip = input('Inserisci indirizzo ip: ')
-porte = input('Inserire un range di porte: ')
+porte = input('Inserire un range di porte (valore minimo 0, valore massimo 65.535): ')
 
 minporta = int(porte.split('-')[0])
 massporta = int(porte.split('-')[1])
 
 
-print('Scansione ip: ', indirizzo_ip, 'Dalla porta', minporta, 'alla porta',massporta)
+print('Scansione IP: ', indirizzo_ip, 'dalla porta', minporta, 'alla porta',massporta)
 
 massporta = int(porte.split('-')[1])+1
 
@@ -15,6 +15,6 @@ for port in range(minporta, massporta ):
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM )
 	status = sock.connect_ex((indirizzo_ip, port))
 	if(status == 0):
-		print('<<< Port',port,'- OPEN >>>',)
+		print('Port',port,'- OPEN')
 
 	sock.close()
